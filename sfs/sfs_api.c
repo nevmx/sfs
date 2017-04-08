1c5065fca33d7878b963fec535203d3b9d6ea028
Maxim Neverov <Maxim Neverov>
2017-04-08 15:43:16 +0100

    Initial commit

---
+++ sfs/sfs_api.c
@@ -0,0 +1,26 @@
+#include "sfs_api.h"
+
+void mkssfs(int fresh){
+
+}
+int ssfs_fopen(char *name){
+    return 0;
+}
+int ssfs_fclose(int fileID){
+    return 0;
+}
+int ssfs_frseek(int fileID, int loc){
+    return 0;
+}
+int ssfs_fwseek(int fileID, int loc){
+    return 0;
+}
+int ssfs_fwrite(int fileID, char *buf, int length){
+    return 0;
+}
+int ssfs_fread(int fileID, char *buf, int length){
+    return 0;
+}
+int ssfs_remove(char *file){
+    return 0;
+}
